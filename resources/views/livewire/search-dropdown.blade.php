44304ade26ebb7572498138bacf27f58897cf36d
Alande Amorim <alande.amorim@gmail.com>
2020-06-02 14:57:21 -0300

    tweaking search dropdown UI interactivity

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,5 +1,14 @@
-<div class="relative">
-    <input wire:model.debounce.500ms="search" type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen: true}" @click.away="isOpen = false">
+    <input
+        wire:model.debounce.500ms="search"
+        type="text"
+        class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
+        placeholder="Search"
+        @focus="isOpen = true"
+        @keydown="isOpen = true"
+        @keydown.escape.window="isOpen = false"
+        @keydown.shift.tab="isOpen = false"
+    >
     <div class="absolute top-0">
         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
     </div>
@@ -8,18 +17,26 @@
 
     @if(strlen($search) > 2)
         @if($results->count() > 0)
-            <ul id="search-results-list" class="absolute bg-gray-800 rounded w-64 mt-2 text-sm overflow-y-auto max-h-1/3 z-10">
+            <ul
+                id="search-results-list"
+                class="absolute bg-gray-800 rounded w-64 mt-2 text-sm overflow-y-auto max-h-1/3 z-10"
+                x-show.transition.opacity="isOpen"
+            >
                 @foreach($results as $movie)
-                <li class="border-b border-gray-700">
-                    <a href="{{ route('movies.show', $movie['id']) }}" class="block hover:bg-gray-700 p-3 flex items-center">
-                        @if($movie['poster_path'])
-                            <img src="https://image.tmdb.org/t/p/w92/{{ $movie['poster_path'] }}" alt="poster" class="w-8">
-                        @else
-                            <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
-                        @endif
-                        <span class="ml-2">{{ $movie['original_title'] }} {{ \Carbon\Carbon::parse($movie['release_date'])->format('(Y)') }}</span>
-                    </a>
-                </li>
+                    <li class="border-b border-gray-700">
+                        <a
+                            href="{{ route('movies.show', $movie['id']) }}"
+                            class="block hover:bg-gray-700 p-3 flex items-center"
+                            @if ($loop->last) @keydown.tab.exact="isOpen = false" @endif
+                        >
+                            @if($movie['poster_path'])
+                                <img src="https://image.tmdb.org/t/p/w92/{{ $movie['poster_path'] }}" alt="poster" class="w-8">
+                            @else
+                                <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+                            @endif
+                            <span class="ml-2">{{ $movie['original_title'] }} {{ \Carbon\Carbon::parse($movie['release_date'])->format('(Y)') }}</span>
+                        </a>
+                    </li>
                 @endforeach
             </ul>
         @else
